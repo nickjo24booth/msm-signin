fce325066af73cbe2ce5249cf083081165d8a423
nickjo24 <njohnso2@chicagobooth.edu>
2023-02-23 03:05:03 +0000

    Have 2 errors left to fix

---
+++ app/models/character.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: characters
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  actor_id   :integer
+#  movie_id   :integer
+#
+class Character < ApplicationRecord
+  belongs_to(:actor, { :class_name => "Actor", :foreign_key => "actor_id" })
+
+  belongs_to(:movie, { :class_name => "Movie", :foreign_key => "movie_id" })
+end
