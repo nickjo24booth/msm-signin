9a575862f2d651589ca2da8d79ae1baa1a51b127
nickjo24 <njohnso2@chicagobooth.edu>
2023-02-23 19:09:28 +0000

    Added some extra things, like cast to the movie show page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,6 +13,9 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  belongs_to(:director, { :class_name => "Director", :foreign_key => "director_id" })
 
-  
+  has_many(:characters, { :class_name => "Character", :foreign_key => :movie_id })
+
+  has_many(:cast, { :through => :characters, :source => :actor })
 end
